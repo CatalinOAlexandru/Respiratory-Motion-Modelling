fa844e8c560cc4996e698eef07be0c0dc52b5c7d
ycgtucl <53170715+platapusdalaputos@users.noreply.github.com>
2021-03-22 20:27:25 +0000

    Adding updates

--- resp_code_part2.m
+++ resp_code_part2.m
@@ -94,41 +94,41 @@ end
 
 % Re-label the variables and matrices to understand whats going on
 
-surrogateSignals = [x_20(101:1500),ones(length(x_20(101:1500)),1)];
-coefficients = C;
-transformations = surrogateSignals*coefficients;
+% linear
+linearsurrogateSignals = [x_20(101:1500),ones(length(x_20(101:1500)),1)];
+linearcoefficients = C;
+lineartransformations = linearsurrogateSignals*linearcoefficients;
+
+% retrieve cpg's
+[cpg1_lin, cpg2_lin] = transForm(lineartransformations,hdr1,hdr2);
+
+%% Polynomial of 2nd order
+
+p2surrogateSignals = [x_20(101:1500).^2,x_20(101:1500),ones(length(x_20(101:1500)),1)];
+p2coefficients = C_p2;
+p2transformations = p2surrogateSignals*p2coefficients;
+
+% retrieve cpg's
+[cpg1_p2, cpg2_p2] = transForm(p2transformations,hdr1,hdr2);
 
 
+%% Polynomial of 3rd order
 
-%% Split into four columns, transpose and reshape
-cpg1_1 = reshape(transformations(1:1400,1:4489)', [67,67,1400,1,1]);
-cpg1_2 = reshape(transformations(1:1400,4490:8978)', [67,67,1400,1,1]);
-cpg2_1 = reshape(transformations(1:1400,8979:13467)', [67,67,1400,1,1]);
-cpg2_2 = reshape(transformations(1:1400,13468:17956)', [67,67,1400,1,1]);
+p3surrogateSignals = [x_20(101:1500).^3, x_20(101:1500).^2,x_20(101:1500),ones(length(x_20(101:1500)),1)];
+p3coefficient = C_p3;
+p3transformations = p3surrogateSignals*p3coefficient;
 
-% Create empty 5D arrays for the for-loop below
-cpg1reshapefinal = zeros(67,67,1400,1,2);
-cpg2reshapefinal = zeros(67,67,1400,1,2);
+% retrieve cpg's
+[cpg1_p3, cpg2_p3] = transForm(p3transformations,hdr1,hdr2);
+%%
 
-% Place the four columns into the empty 5D arrays
-for i = 1:1400
-    cpg1reshapefinal(:,:,i,1,1)=cpg1_1(:,:,i);
-    cpg1reshapefinal(:,:,i,1,2)=cpg1_2(:,:,i);
-    cpg2reshapefinal(:,:,i,1,1)=cpg2_1(:,:,i);
-    cpg2reshapefinal(:,:,i,1,2)=cpg2_2(:,:,i);
-end
 
-%% Create the transformations structs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
+                   % Linear Deformation Fielf Error %
 
-% Create structs like the old cpg1 and cpg2 for the deformation function
-Z = 1;
-for i = 1:1400
-    cpg1_new(Z).img = cpg1reshapefinal(:,:,i,:,:);
-    cpg1_new(Z).hdr = hdr1(:,i);
-    cpg2_new(Z).img = cpg2reshapefinal(:,:,i,:,:);
-    cpg2_new(Z).hdr = hdr2(:,i);
-    Z=Z+1;
-end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 %% Model Images
 
@@ -138,7 +138,7 @@ Z = 1;
 for i = 1:1400
     
         [modImg(Z), modDef(Z), modDis(Z)] = ...
-            deformNiiWithCPGsSliding(cpg1_new(:,i), cpg2_new(:,i), SDMS, source_image, images(:,i+100));
+            deformNiiWithCPGsSliding(cpg1_lin(:,i), cpg2_lin(:,i), SDMS, source_image, images(:,i+100));
 
         Z = Z+1;
 end
@@ -152,7 +152,7 @@ Z = 1;
 for i = 1:1400
     
         [mDSMI(Z), mDSMI_def(Z), mDSMI_dis(Z)] = ...
-            deformNiiWithCPGsSliding(cpg1_new(:,i), cpg2_new(:,i), SDMS, source_mask, images(:,i+100));
+            deformNiiWithCPGsSliding(cpg1_lin(:,i), cpg2_lin(:,i), SDMS, source_mask, images(:,i+100));
 
         Z = Z+1;
 end
@@ -209,7 +209,7 @@ end
 
 figure(3);
 subplot(1,3,1)
-dispNiiSlice(DEF_def(:,1),"z",1)
+dispNiiSlice(L2_norm(:,1),"z",1)
 
 subplot(1,3,2)
 dispNiiSlice(DEF_def(:,500),"z",1)
@@ -217,7 +217,53 @@ dispNiiSlice(DEF_def(:,500),"z",1)
 subplot(1,3,3)
 dispNiiSlice(DEF_def(:,1000),"z",1)
 
+%% Calculating the L2 norm of each image
+
+L2_norm = DEF_def;
+mean_Val = [];
+
+% histo=zeros(10,160,160);
+
+for i = 1:1400
+    for j = 1:160
+        for k = 1:160
+            % Registration Deformation field minus Model Deformation field
+            L2_norm(i).img(j,k) = sqrt((rDSMI_def(:,i).img(j,k) - modDef(:,i).img(j,k))^2);
+            
+            % tried to remove all the NaN values and failed miserably
+            if isnan(L2_norm(i).img(j,k))
+                L2_norm(i).img(j,k)=0;
+            end
+            
+         
+        end 
+    end
+end
+%% Other statistical measures
+
+% measure the average error value per image
+
+mean_Val = [];
+
+for i =1:1400
+    
+    SimpleArray = L2_norm(:,i).img;
+    NewSimpleArray = SimpleArray(SimpleArray ~= 0);
+    mean_Val(i) = nanmean(NewSimpleArray);
+    
+end
+
+% Measure the error in the Superior Inferior (SI) position:
 
 
+% Measure the error in the Anterior Posterior (AP) position
 
+%% Plot Average value against corresponding signal
+
+figure(3);
+subplot(1,2,1)
+plot(mean_Val);
+
+subplot(1,2,2)
+plot(x_20(100:1500,1));
 
